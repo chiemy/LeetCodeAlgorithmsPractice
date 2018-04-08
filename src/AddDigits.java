160414ef300ee288c41d8407a0898efc08d23d72
chiemy <luobaolong@gmail.com>
2018-04-08 23:21:39 +0800

    max area of island

--- src/AddDigits.java
+++ src/AddDigits.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+/**
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
 
+ For example:
+
+ Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ */
 public class AddDigits {
+    public int addDigits(int num) {
+        int sum = 0;
+        while(num > 10) {
+            sum = 0;
+            while(num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return sum;
+    }
 }
