4f57b26e0be4f702600d860e529d1cdcb01131d9
chiemy <luobaolong@gmail.com>
2018-04-12 23:01:00 +0800

    another solution

--- src/FindTheDifference.java
+++ src/FindTheDifference.java
@@ -10,4 +10,15 @@ public class FindTheDifference {
         }
         return difference;
     }
+
+    public char findTheDifference2(String s, String t) {
+        char c = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            c ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
 }
